938c75959cb70b2f588a2f8269f29a35ca1a9f73
Louis <loulamia@Louiss-Eximchain-MacBook-Pro.local>
2018-03-19 10:44:41 -0400

    Don't kill the server on failed getKey call

--- service.go
+++ service.go
@@ -23,7 +23,6 @@ func (svc transactionExecutorService) GetKey(_ context.Context) (string, error) 
 	vault := svc.vaultClient.Logical()
 	secret, err := vault.Read(pathArg)
 	if err != nil {
-		log.Fatal(err)
 		return "", ErrVault
 	}
 	return secret.Data["key"].(string), nil
